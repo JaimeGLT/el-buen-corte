804cdaf46ad4261bd00387796bfe0964a12cac19
jaimeGLT <jaimeguzmanlt@gmail.com>
2025-11-18 06:42:26 -0400

    chat added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import VoiceflowChat from './components/Chat.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    {/* <VoiceflowChat /> */}
   </BrowserRouter>,
 )
